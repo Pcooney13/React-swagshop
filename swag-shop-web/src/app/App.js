3e3e3235a9a22bca00e7a41a018b2f7ea9a5aadd
Patrick Cooney <pat.cooney13@gmail.com>
2018-07-24 12:45:25 -0400

    Add files via upload
    finished 'Devslopes' practice shopping cart/ wishlist.

--- swag-shop-web/src/app/App.js
+++ swag-shop-web/src/app/App.js
@@ -34,9 +34,9 @@ class App extends Component {
   }
 
   productList = () => {
-    const list = this.state.products.map((product) => 
+    const list = this.state.products.map((product) =>
       <div className="col-sm-4 product-holder" key={product._id}>
-        <Product title={product.title} price={product.price} imgUrl={product.imgUrl} />
+        <Product product={product} />
       </div>
     )
     return(list);
@@ -54,7 +54,7 @@ class App extends Component {
             <div className="col-sm-8">
               <div className="row">
                 {this.productList()}
-              </div>     
+              </div>
             </div>
             <div className="col-sm-4">
             <WishList />
--- swag-shop-web/src/product-condensed/product-condensed.js
+++ swag-shop-web/src/product-condensed/product-condensed.js
@@ -1,11 +1,27 @@
 import React, {Component} from 'react';
 import './product-condensed.css';
+import DataService from '../services/data-service';
+
+let ds = new DataService();
 
 class ProductCondensed extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.removeProduct = this.removeProduct.bind(this);
+    }
+
+    removeProduct = () => {
+        ds.removeWishListItem(this.props.product);
+    }
+
+
+
     render() {
         return(
-            <li className="list group-item">
-                <a className="btn btn-outline-danger">{this.props.product.title} | ${this.props.product.price}</a>
+            <li className="list-group-item pc-list">
+                <a className="btn btn-outline-danger x-button" onClick={() => this.removeProduct()}>x</a><span>{this.props.product.title} | <b>${this.props.product.price}</b></span>
             </li>
         );
     }
